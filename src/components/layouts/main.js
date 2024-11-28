39a84385b2e6b19e719472636be643f33dca4107
yudhistiaR <Yudhistia110@gmail.com>
2024-11-28 15:58:20 +0800

    change(style) : padding main layout

--- src/components/layouts/main.js
+++ src/components/layouts/main.js
@@ -1,6 +1,6 @@
 const MainLayout = ({ children }) => {
   return (
-    <section className="m-auto w-full h-full flex flex-col justify-center items-center max-w-xl min-h-screen bg-gray-100/90 relative overflow-x-scroll py-16 px-4">
+    <section className="m-auto w-full h-full flex flex-col justify-center items-center max-w-xl min-h-screen bg-gray-100/90 relative overflow-x-hidden py-20 px-4">
       {children}
     </section>
   );
