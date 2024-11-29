33920d62e4bc8b53175e38ac3d2a168bb636ca3d
yudhistiaR <Yudhistia110@gmail.com>
2024-11-29 09:26:05 +0800

    change(middleware) : add middleware protec routes

--- src/middleware.js
+++ src/middleware.js
@@ -1,18 +1,24 @@
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
+import * as jose from 'jose';
 
-const protectedRoutes = ['/', '/ti', '/si', '/dashboard'];
-//const publicRoutes = ['/login', '/register'];
+const protectedRoutes = ['/', '/dashboard', '/admin'];
 
 export default async function middleware(req) {
   const path = req.nextUrl.pathname;
   const isProtectedRoutes = protectedRoutes.includes(path);
-  //const isPublicRoutes = publicRoutes.includes(path);
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
-  const token = (await cookies()).get('token')?.value;
+  const token = await cookies().get('token')?.value;
+
+  const { payload: decoded } = jose.jwtVerify(token, secret);
 
   if (isProtectedRoutes && token === undefined) {
     return NextResponse.redirect(new URL('/login', req.nextUrl));
+  }
+
+  if (isProtectedRoutes && decoded?.status_akun !== 'Pelaksana') {
+    return NextResponse.redirect(new URL('/', req.nextUrl));
   }
 
   return NextResponse.next();
