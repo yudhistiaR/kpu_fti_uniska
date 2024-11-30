e69162f8261d90a29f6cb3bf72fe3a64cc783431
yudhistiaR <Yudhistia110@gmail.com>
2024-11-30 10:40:23 +0800

    fix(middleware) : add dadmin route protect

--- src/middleware.js
+++ src/middleware.js
@@ -2,13 +2,15 @@ import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import * as jose from 'jose';
 
-const protectedRoutes = ['/', '/dashboard', '/admin'];
+const adminRoutes = ['/dashboard', '/admin'];
+const protectedRoutes = ['/', '/ti', '/si'];
 const publicRoutes = ['/login', '/register'];
 
 export default async function middleware(req) {
   const path = req.nextUrl.pathname;
   const isProtectedRoutes = protectedRoutes.includes(path);
   const isPublicRoutes = publicRoutes.includes(path);
+  const isAdminRoutes = adminRoutes.includes(path);
 
   const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
@@ -20,6 +22,12 @@ export default async function middleware(req) {
   }
 
   if (isPublicRoutes && token) {
+    return NextResponse.redirect(new URL('/', req.nextUrl), {
+      status: 307
+    });
+  }
+
+  if (isAdminRoutes && token) {
     try {
       const { payload: decoded } = await jose.jwtVerify(token, secret);
       if (decoded?.status_akun !== 'Pelaksana') {
@@ -34,7 +42,6 @@ export default async function middleware(req) {
         });
       }
     }
-
     return NextResponse.next();
   }
 
