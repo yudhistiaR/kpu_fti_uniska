09de50d1a2b8985c9c8b0235c26554cd16248d14
yudhistiaR <Yudhistia110@gmail.com>
2024-11-30 10:22:01 +0800

    fix(middleware) : fix infinit loop middlewr

--- src/middleware.js
+++ src/middleware.js
@@ -3,22 +3,39 @@ import { NextResponse } from 'next/server';
 import * as jose from 'jose';
 
 const protectedRoutes = ['/', '/dashboard', '/admin'];
+const publicRoutes = ['/login', '/register'];
 
 export default async function middleware(req) {
   const path = req.nextUrl.pathname;
   const isProtectedRoutes = protectedRoutes.includes(path);
+  const isPublicRoutes = publicRoutes.includes(path);
+
   const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
-  const token = await cookies().get('token')?.value;
-
-  const { payload: decoded } = jose.jwtVerify(token, secret);
-
-  if (isProtectedRoutes && token === undefined) {
-    return NextResponse.redirect(new URL('/login', req.nextUrl));
+  const token = (await cookies()).get('token')?.value;
+  if (isProtectedRoutes && !token && token === undefined) {
+    return NextResponse.redirect(new URL('/login', req.nextUrl), {
+      status: 307
+    });
   }
 
-  if (isProtectedRoutes && decoded?.status_akun !== 'Pelaksana') {
-    return NextResponse.redirect(new URL('/', req.nextUrl));
+  if (isPublicRoutes && token) {
+    try {
+      const { payload: decoded } = await jose.jwtVerify(token, secret);
+      if (decoded?.status_akun !== 'Pelaksana') {
+        return NextResponse.redirect(new URL('/', req.nextUrl), {
+          status: 307
+        });
+      }
+    } catch (error) {
+      if (error) {
+        return NextResponse.redirect(new URL('/login', req.nextUrl), {
+          status: 307
+        });
+      }
+    }
+
+    return NextResponse.next();
   }
 
   return NextResponse.next();
