97f58ff7cebe87eec6910c69ba73411f0ae647f7
yudhistiaR <Yudhistia110@gmail.com>
2024-12-05 15:29:40 +0800

    fix(build) : error build

--- src/app/admin/calon/list/page.js
+++ src/app/admin/calon/list/page.js
@@ -2,10 +2,10 @@ import ListCalon from '@/components/admin/ListCalon';
 
 const ListCalonPage = () => {
   return (
-    <section>
+    <>
       <h1 className="text-2xl font-bold mb-4">List Calon</h1>
       <ListCalon />
-    </section>
+    </>
   );
 };
 
--- src/components/admin/ListCalon.js
+++ src/components/admin/ListCalon.js
@@ -1,3 +1,4 @@
+import dynamic from 'next/dynamic';
 import {
   Table,
   TableBody,
@@ -9,14 +10,15 @@ import {
 } from '@/components/ui/table';
 import Image from 'next/image';
 import parser from 'html-react-parser';
-import DeleteCalon from '../action/deleteCalon';
 import { toast } from 'sonner';
+
+const DeleteCalon = dynamic(() => import('../action/deleteCalon'));
 
 const ListCalon = async () => {
   const fetchDataCalon = async () => {
     return await fetch(`${process.env.API_URL}/api/v1/calon`, {
       method: 'GET',
-      next: { revalidate: 0 }
+      next: { cache: 'force-cache' }
     }).then(async res => {
       if (!res.ok) {
         const error = await res.json();
@@ -31,52 +33,50 @@ const ListCalon = async () => {
   const { data } = await fetchDataCalon();
 
   return (
-    <section>
-      <div className="w-full p-4 bg-white rounded-lg shadow-md">
-        <Table>
-          <TableCaption>List Calon</TableCaption>
-          <TableHeader>
-            <TableRow className="text-center">
-              <TableHead>NO</TableHead>
-              <TableHead>Foto</TableHead>
-              <TableHead>Nama</TableHead>
-              <TableHead>No Urut</TableHead>
-              <TableHead>Prodi</TableHead>
-              <TableHead>Angkatan</TableHead>
-              <TableHead>Jenis Pencalonan</TableHead>
-              <TableHead>{'Visi & Misi'}</TableHead>
-              <TableHead>Action</TableHead>
+    <div className="w-full p-4 bg-white rounded-lg shadow-md">
+      <Table>
+        <TableCaption>List Calon</TableCaption>
+        <TableHeader>
+          <TableRow className="text-center">
+            <TableHead>NO</TableHead>
+            <TableHead>Foto</TableHead>
+            <TableHead>Nama</TableHead>
+            <TableHead>No Urut</TableHead>
+            <TableHead>Prodi</TableHead>
+            <TableHead>Angkatan</TableHead>
+            <TableHead>Jenis Pencalonan</TableHead>
+            <TableHead>{'Visi & Misi'}</TableHead>
+            <TableHead>Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.map((item, idx) => (
+            <TableRow key={item.id} className="text-center">
+              <TableCell className="font-medium">{idx + 1}</TableCell>
+              <TableCell className="font-medium">
+                <Image
+                  alt="Foto calon"
+                  src={item.foto}
+                  width={80}
+                  height={80}
+                />
+              </TableCell>
+              <TableCell className="font-medium">{item.nama}</TableCell>
+              <TableCell className="font-medium">{item.no_urut}</TableCell>
+              <TableCell className="font-medium">{item.prodi}</TableCell>
+              <TableCell className="font-medium">{item.angkatan}</TableCell>
+              <TableCell className="font-medium">{item.type}</TableCell>
+              <TableCell className="font-medium text-justify">
+                {parser(item.visiMisi)}
+              </TableCell>
+              <TableCell className="font-medium flex space-x-2 justify-center text-white">
+                <DeleteCalon calonId={item.id} />
+              </TableCell>
             </TableRow>
-          </TableHeader>
-          <TableBody>
-            {data.map((item, idx) => (
-              <TableRow key={item.id} className="text-center">
-                <TableCell className="font-medium">{idx + 1}</TableCell>
-                <TableCell className="font-medium">
-                  <Image
-                    alt="Foto calon"
-                    src={item.foto}
-                    width={80}
-                    height={80}
-                  />
-                </TableCell>
-                <TableCell className="font-medium">{item.nama}</TableCell>
-                <TableCell className="font-medium">{item.no_urut}</TableCell>
-                <TableCell className="font-medium">{item.prodi}</TableCell>
-                <TableCell className="font-medium">{item.angkatan}</TableCell>
-                <TableCell className="font-medium">{item.type}</TableCell>
-                <TableCell className="font-medium text-justify">
-                  {parser(item.visiMisi)}
-                </TableCell>
-                <TableCell className="font-medium flex space-x-2 justify-center text-white">
-                  <DeleteCalon calonId={item.id} />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
-    </section>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   );
 };
 
