567832b3f286fc792ca9985560265d7b5b9807af
yudhistiaR <Yudhistia110@gmail.com>
2024-12-08 14:40:04 +0800

    feat(log) : add logger file error & info

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,7 +1,25 @@
-import winston from "winston";
+import winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
 
 export const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.json(),
-  transports: [new winston.transports.Console()],
+  level: 'info',
+  format: winston.format.printf(log => {
+    return `${log.level.toUpperCase()} : ${JSON.stringify(log.message)}`;
+  }),
+  transports: [
+    new DailyRotateFile({
+      level: 'error',
+      filename: 'log/application-error-%DATE%.log',
+      zippedArchive: true,
+      maxSize: '1m',
+      maxFiles: '14d'
+    }),
+    new DailyRotateFile({
+      level: 'info',
+      filename: 'log/application-%DATE%.log',
+      zippedArchive: true,
+      maxSize: '2m',
+      maxFiles: '14d'
+    })
+  ]
 });
