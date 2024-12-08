60f6889a4092fb9c1f8c0ca1e02778727a419aa2
yudhistiaR <Yudhistia110@gmail.com>
2024-12-08 18:55:28 +0800

    fix error

--- src/app/admin/calon/list/page.js
+++ src/app/admin/calon/list/page.js
@@ -1,11 +1,13 @@
+'use client';
+
 import ListCalon from '@/components/admin/ListCalon';
 
 const ListCalonPage = () => {
   return (
-    <>
+    <section>
       <h1 className="text-2xl font-bold mb-4">List Calon</h1>
       <ListCalon />
-    </>
+    </section>
   );
 };
 
